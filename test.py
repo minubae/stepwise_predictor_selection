13263cdba7121ce5ef3ec70051a639aac97916c2
minwoobae <minubae.nyc@gmail.com>
2018-05-10 22:32:14 -0400

    modified: test.py

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
+import csv
 import numpy as np
+max_row = 9358
+data = np.genfromtxt('AirQualityUCI/AirQualityUCI.csv', delimiter=';', usecols = range(2,15), skip_header = 1, dtype=float, loose = True, max_rows = max_row) #, max_rows = 10
+
+data = np.array(data)
+Y = data[:,3]
+# print(Y)
+
+for y in Y:
+    print(y)
+# for d in data:
+#     print(d)
+# print('hello')

3163715b6a2715dbd5d12e3d43ad5d985146d03c
minwoobae <minubae.nyc@gmail.com>
2018-05-11 22:14:48 -0400

    added: some comments on codes

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 ###########################################################################################################################
 # Title: Stepwise Predictors Selection Method
-# Course: Math B7800 - Advanced Mathematical Statistics
-# Date: May/15/2018, Wednesday
+# Course: Math B7800 - Advanced Mathematical Statistics II
+# Instructor: Professor Shirshendu Chatterjee
+# Due Date: May/15/2018, Wednesday
 # Author: Minwoo Bae (minubae.math@gmail.com)
 # Institute: The Department of Mathematics, City College of New York, CUNY
 
@@ -10,15 +11,16 @@
 # Carry out Stepwise Variable selection method to select an appropriate subset of assumptions.
 # Use AIC, Adjusted R^2, and C_p plot to select suitable subset of variable.
 
-# Data Set Information (Air Quiality Data Set from UCI Machine Learning Respository):
+# Data Set Information:
 # The dataset contains 9358 instances of hourly averaged responses from an array of 5 metal oxide chemical sensors embedded
 # in an Air Quality Chemical Multisensor Device. The device was located on the field in a significantly polluted area,
 # at road level,within an Italian city. Data were recorded from March 2004 to February 2005 (one year)representing
 # the longest freely available recordings of on field deployed air quality chemical sensor devices responses.
 # Ground Truth hourly averaged concentrations for CO, Non Metanic Hydrocarbons, Benzene, Total Nitrogen Oxides (NOx) and
 # Nitrogen Dioxide (NO2) and were provided by a co-located reference certified analyzer. Evidences of cross-sensitivities
-# as well as both concept and sensor drifts are present as described in De Vito et al., Sens. And Act. B, Vol. 129,2,2008 (citation required)
-# eventually affecting sensors concentration estimation capabilities. Missing values are tagged with -200 value.
+# as well as both concept and sensor drifts are present as described in De Vito et al., Sens. And Act. B, Vol. 129,2,2008
+# (citation required)eventually affecting sensors concentration estimation capabilities.
+# Missing values are tagged with -200 value.
 # This dataset can be used exclusively for research purposes. Commercial purposes are fully excluded.
 
 # Data Set Sources: https://archive.ics.uci.edu/ml/datasets/Air+quality
@@ -34,18 +36,17 @@ end = 20 #9358
 ###################################################################################
 # Import data from a CSV file: 'AirQualityUCI/AirQualityUCI.csv'
 ###################################################################################
-data = np.genfromtxt('AirQualityUCI/AirQualityUCI.csv', delimiter=';', usecols = range(2,15), skip_header = 1, dtype=float, loose = True, max_rows = end) #, max_rows = 10
+
+csv_url = 'AirQualityUCI/AirQualityUCI.csv'
+data = np.genfromtxt(csv_url, delimiter=';', usecols = range(2,15), skip_header = 1, dtype=float, loose = True, max_rows = end)
 data = np.array(data, dtype=float)
 
-# Get a Response vector Y from the data
+# Get the Benzene concentration as a Response vector Y from the data
 Y = data[begin:end,3]
 
 # Get the number of observations
 n = Y.shape[0]
 print('n:', n)
-# print('Y:', Y)
-# Z = data[0:n,10:12]
-# Z = data[0:n,[0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12]]
 
 # Get a Data Matrix Z from the data
 Z = np.delete(data,3,axis=1)[begin:end,] # axis=1 -- select a column, axis=0 -- select a row.
